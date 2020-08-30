3d48ac9beac8198776778eee7be39df84f0ea306
Guilherme Cartier <gcartier@br.ibm.com>
2020-08-30 19:00:13 -0300

    Adding initial Pyinquirer test
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- gman/gman.py
+++ gman/gman.py
@@ -3,7 +3,17 @@ import os
 from github import Github
 from config import local_config
 
-if __name__ == '__main__':
+from PyInquirer import style_from_dict, Token, prompt, Separator
+from examples import custom_style_2
+
+
+
+
+
+
+
+
+def display_all_repos_in_org():
     github_config = local_config.github_config
     access_token = github_config['access_token']
     github_url = github_config['base_url']
@@ -13,3 +23,20 @@ if __name__ == '__main__':
     repo_list = org_object.get_repos()
     for repo in repo_list:
         print(repo.name)
+
+
+if __name__ == '__main__':
+    temp_file = open('./temp_stash.txt', 'r')
+    stash_records = temp_file.readlines()
+    choices = [{'name': repo_name} for repo_name in stash_records]
+    question = [
+    {
+        'type': 'checkbox',
+        'qmark': '🛠',
+        'message': 'Select an item',
+        'name': 'repositories',
+        'choices': choices
+    }]
+
+    answer = prompt(question, style=custom_style_2)
+    print(answer)
